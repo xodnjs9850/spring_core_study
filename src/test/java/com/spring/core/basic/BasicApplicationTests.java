acd46c3dc2946b763379588e4717bb6c28c3af15
xodnjs9850 <xodnjs9850@naver.com>
2021-04-27 15:58:26 +0900

    first commit

---
+++ src/test/java/com/spring/core/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.core.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
